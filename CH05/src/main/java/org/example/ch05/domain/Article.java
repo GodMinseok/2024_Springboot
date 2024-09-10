58c36272db7333a102d3f8190cf2bd1651020db8
GodMinseok <mseok830@naver.com>
2024-09-10 10:52:46 +0900

    CH05 Start

---
+++ CH05/src/main/java/org/example/ch05/domain/Article.java
@@ -0,0 +1,42 @@
+package org.example.ch05.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
