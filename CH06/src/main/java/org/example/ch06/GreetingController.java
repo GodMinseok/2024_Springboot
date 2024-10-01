1d6e4057b1fe42b19ea5fcdce81199cd52619b51
GodMinseok <mseok830@naver.com>
2024-10-01 12:01:08 +0900

    로깅 수준에 따라서 변경

--- CH06/src/main/java/org/example/ch06/GreetingController.java
+++ CH06/src/main/java/org/example/ch06/GreetingController.java
@@ -1,15 +1,19 @@
 package org.example.ch06;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GreetingController {
 
-
+//    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
     private final Greeting greeting;
     private final Droid droid;
 
@@ -38,6 +42,8 @@ public class GreetingController {
 
     @GetMapping("/coffee")
     public String coffee() {
+        log.trace("트레이스 로그인입니다.");
+        log.info("인포 로그입니다.");
         return greeting.getCoffee();
     }
 
