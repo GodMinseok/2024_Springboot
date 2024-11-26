7eb762d36e009a625f0d2ebb38f4c7b3f4ae6743
GodMinseok <mseok830@naver.com>
2024-11-26 10:48:02 +0900

    User 생성

---
+++ CH11/src/main/java/org/example/ch11/User.java
@@ -0,0 +1,13 @@
+package org.example.ch11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+}
