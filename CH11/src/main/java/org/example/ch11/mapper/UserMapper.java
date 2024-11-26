c89aa157f375d8d984628da0bc226c97bb2df019
GodMinseok <mseok830@naver.com>
2024-11-26 11:33:08 +0900

    동적 SQL

--- CH11/src/main/java/org/example/ch11/mapper/UserMapper.java
+++ CH11/src/main/java/org/example/ch11/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package org.example.ch11.mapper;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.example.ch11.User;
 
 @Mapper
@@ -13,4 +14,7 @@ public interface UserMapper {
     void insertUser(User user);
     void updateUser(User user);
     void deleteUser(Long id);
+
+    List<User> findByCondition(@Param("name") String name, @Param("email") String email);
+    List<User> findByIds(@Param("ids") List<Long> ids);
 }
--- CH11/src/main/java/org/example/ch11/service/UserService.java
+++ CH11/src/main/java/org/example/ch11/service/UserService.java
@@ -32,4 +32,6 @@ public class UserService {
         userMapper.deleteUser(id);
     }
 
+
+
 }
